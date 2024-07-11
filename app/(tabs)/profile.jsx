2dbb5976cdc001ea0249aba3af8e5627f956e823
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-11 12:25:10 +0300

    profile

---
+++ app/(tabs)/profile.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Profile
+    </Text>
+    </View>
+  )
+}
+
+export default Profile
