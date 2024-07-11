71f524bbc9bab5c998f2dbe289b9ff7d47898e6a
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-11 12:22:32 +0300

    index

---
+++ app/index.jsx
@@ -0,0 +1,31 @@
+import { Link } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Native 5</Text>
+      <StatusBar style="auto" />
+      <Link href="/home" style={styles.link}>Home</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  link: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+  },
+});
