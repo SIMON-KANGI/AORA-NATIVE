279b217b5d49b066d142cad76060389ec2539a12
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-12 14:32:56 +0300

    layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -24,7 +24,10 @@ if(fontsLoaded) SplashScreen.hideAsync()
   },[fontsLoaded, error])
   return (
    <Stack>
+   <Stack.Screen name='index' options={{headerShown:false}}/>
     <Stack.Screen name='(tabs)' options={{headerShown:false}}/>
+    <Stack.Screen name='(auth)' options={{headerShown:false}}/>
+    {/* <Stack.Screen name='/search/[query]' options={{headerShown:false}}/> */}
    </Stack>
   )
 }
